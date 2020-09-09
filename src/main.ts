16e491c6e090421d1e87c718f5852620f2411e28
Bora Baloglu <baloglub@gmail.com>
2020-09-09 16:38:56 +0300

    feat(src/main.ts): global validation pipe is added
    validation will be done in every single endpoint, so global pipe is used

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ValidationError } from 'class-validator';
 
 import envConfig from 'src/shared/configs/env.config';
 
+import { ServerError, ServerErrorType } from 'src/shared/configs/errors.config';
 import { GlobalExceptionFilter } from 'src/shared/filters/exception.filter';
 
 import { AppModule } from 'src/app.module';
@@ -20,6 +23,20 @@ import { AppModule } from 'src/app.module';
 
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastify);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (validationErrors: ValidationError[]) => {
+        const errorMessages: string[] = [];
+        validationErrors.forEach((error: ValidationError) => {
+          Object.keys(error.constraints).forEach((key: string) => {
+            errorMessages.push(error.constraints[key]);
+          });
+        });
+        return new ServerError(ServerErrorType.GIVEN_INPUT_IS_INVALID, errorMessages.join(','));
+      },
+    }),
+  );
 
   await app.listen(envConfig.port, process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1');
 })();
