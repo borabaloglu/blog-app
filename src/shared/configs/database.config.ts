66d4866f4c94b1eed3bb7b2c076a9638ebb8e448
Bora Baloglu <baloglub@gmail.com>
2020-09-09 18:17:39 +0300

    feat(src/shared/configs/database.config.ts): database configurations are added

---
+++ src/shared/configs/database.config.ts
@@ -0,0 +1,33 @@
+import { SequelizeModuleOptions } from '@nestjs/sequelize';
+
+import * as helpers from 'src/shared/helpers/config.helper';
+
+const config: SequelizeModuleOptions = {
+  dialect: 'postgres',
+  host: helpers.loadEnvOrError('DB_HOST'),
+  port: helpers.loadEnvOrError('DB_PORT', true),
+  logging: process.env.NODE_ENV === 'development',
+  native: true,
+  pool: {
+    max: 5,
+    idle: 30000,
+    acquire: 60000,
+  },
+  define: {
+    underscored: false,
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+  },
+  database: helpers.loadEnvOrError('DB_NAME'),
+  username: helpers.loadEnvOrError('DB_USERNAME'),
+  password: helpers.loadEnvOrError('DB_PASSWORD'),
+  models: ['modules/**/entities/*{.js,.ts}'],
+  sync: {
+    force: false,
+    alter: true,
+  },
+  synchronize: true,
+  autoLoadModels: true,
+};
+
+export default config;
