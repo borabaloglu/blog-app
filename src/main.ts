714d7563e1f0cf76941ecc18de6a75579be80c5c
Bora Baloglu <baloglub@gmail.com>
2020-09-09 16:25:09 +0300

    feat(*): project structure is generated

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from 'src/app.module';
+
+(async () => {
+  const fastify = new FastifyAdapter({
+    logger: {
+      file: process.env.NODE_ENV === 'production' ? `./logs/master.log` : undefined,
+      level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
+      useLevelLabels: true,
+      prettyPrint:
+        process.env.NODE_ENV === 'production' ? false : { translateTime: true, levelFirst: true },
+    } as any,
+  });
+
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastify);
+
+  await app.listen(3000, process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1');
+})();
